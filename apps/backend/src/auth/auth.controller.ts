fccc7fb670828672260734ba79d45f615169af7b
HyeongSeoku <gudtjr3437@naver.com>
2024-02-22 20:11:33 +0900

    [BE] Modify : 회원가입 및 로그인 시도 분당 제한

--- apps/backend/src/auth/auth.controller.ts
+++ apps/backend/src/auth/auth.controller.ts
@@ -12,10 +12,9 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { CreateUserDto } from 'src/users/dto/create-user.dto';
 import { UsersService } from 'src/users/users.service';
-import { Users } from 'src/database/entities/user.entity';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { LoginDto } from 'src/users/dto/login-user.dto';
 import { AuthenticatedRequest } from './auth.interface';
+import { RateLimit } from 'nestjs-rate-limiter';
 
 @Controller('auth')
 export class AuthController {
@@ -24,12 +23,14 @@ export class AuthController {
     private readonly usersService: UsersService,
   ) {}
 
+  @RateLimit({ points: 10, duration: 60 })
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req: AuthenticatedRequest) {
     return this.authService.login(req);
   }
 
+  @RateLimit({ points: 2, duration: 60 })
   @Post('signup')
   async signup(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
