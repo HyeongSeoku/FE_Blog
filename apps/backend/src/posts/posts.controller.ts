bbef43aacd09a7a7a76dddf90d5e23dbc5de228a
HyeongSeoku <gudtjr3437@naver.com>
2024-03-08 18:59:25 +0900

    [BE] Modify : Post Create 기능 admin guard 테스트 중

--- apps/backend/src/posts/posts.controller.ts
+++ apps/backend/src/posts/posts.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, Get, Logger, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Logger,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { AdminGuard } from 'src/guards/admin-auth.guard';
 import { CreatePostDto } from './dto/post.dto';
@@ -14,9 +22,10 @@ export class PostsController {
     return this.postsService.findAll();
   }
 
-  //   @UseGuards(AdminGuard)
+  @UseGuards(AdminGuard)
   @Post('create')
-  newPost(@Body() createPostDto: CreatePostDto) {
+  newPost(@Request() req, @Body() createPostDto: CreatePostDto) {
+    this.logger.log('TEST', req?.user);
     return this.postsService.createPost(createPostDto);
   }
 }
