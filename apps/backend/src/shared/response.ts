36e37540dfb60bcc74dc724b81d6b4010bec5282
HyeongSeoku <gudtjr3437@naver.com>
2024-03-18 13:39:48 +0900

    [BE] Add : 기본 응답 클래스 정의

---
+++ apps/backend/src/shared/response.ts
@@ -0,0 +1,11 @@
+export interface SuccessResponseConstructor {
+  message?: string;
+}
+
+export class SuccessResponse {
+  message: string;
+
+  constructor({ message = 'Success' }: SuccessResponseConstructor) {
+    this.message = message;
+  }
+}
