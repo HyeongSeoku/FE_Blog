d86bb07e4ec0d9fcec6bf602b86c326b84461106
HyeongSeoku <gudtjr3437@naver.com>
2024-03-20 16:23:00 +0900

    [BE] Add : comments entity 추가

--- apps/backend/src/database/entities/comments.entity.ts
+++ apps/backend/src/database/entities/comments.entity.ts
@@ -1,15 +1,18 @@
 import {
+  AfterLoad,
   Column,
   Entity,
   JoinColumn,
   ManyToOne,
+  OneToMany,
   PrimaryGeneratedColumn,
 } from 'typeorm';
 import { Posts } from './posts.entity';
+import { Users } from './user.entity';
 
 @Entity('comments')
 export class Comments {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn({ name: 'comment_id' })
   commentId: number;
 
   @Column({ name: 'created_at' })
@@ -19,9 +22,30 @@ export class Comments {
   updatedAt: Date;
 
   @Column()
-  body: string;
+  content: string;
 
   @ManyToOne(() => Posts)
   @JoinColumn({ name: 'post_id' })
-  posts: Posts;
+  post: Posts;
+
+  @Column({ type: 'tinyint', name: 'is_deleted' })
+  is_deleted: number;
+
+  @AfterLoad()
+  setIsDeleted() {
+    this.isDeleted = this.is_deleted === 1;
+  }
+
+  isDeleted: boolean;
+
+  @ManyToOne(() => Users)
+  @JoinColumn({ name: 'user_id' })
+  user: Users;
+
+  @OneToMany(() => Comments, (comments) => comments.parent)
+  replies: Comments[];
+
+  @ManyToOne(() => Comments, (comments) => comments.replies)
+  @JoinColumn({ name: 'parent_comment_id' })
+  parent: Comments;
 }
