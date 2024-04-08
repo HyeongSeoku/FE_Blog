9301c405b7968077a52a97bc8903a494d553acfd
HyeongSeok <gudtjr3437@naver.com>
2024-04-09 01:10:41 +0900

    [BE] Test : 테스트 코드 기본 수정

--- apps/backend/src/comments/comments.service.spec.ts
+++ apps/backend/src/comments/comments.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CommentsService } from './comments.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Comments } from 'src/database/entities/comments.entity';
 
 describe('CommentsService', () => {
   let service: CommentsService;
+  const mockCommentsRepository = {
+    create: jest.fn(),
+    save: jest.fn(),
+    findOne: jest.fn(),
+    update: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CommentsService],
+      providers: [
+        CommentsService,
+        {
+          provide: getRepositoryToken(Comments),
+          useValue: mockCommentsRepository,
+        },
+      ],
     }).compile();
 
     service = module.get<CommentsService>(CommentsService);
