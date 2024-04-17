2eb8f1a5240a6817ea57b91d457ea510eaa49378
HyeongSeoku <gudtjr3437@naver.com>
2024-04-17 10:52:25 +0900

    [BE] Modify : DB 마이그레이션 파일 추가

--- apps/backend/src/database/database.module.ts
+++ apps/backend/src/database/database.module.ts
@@ -15,6 +15,9 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         database: configService.get('DB_DATABASE'),
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
         logging: process.env.NODE_ENV !== 'production',
+        migrations: [__dirname + '/../migrations/**/*{.ts,.js}'], // 마이그레이션 파일 경로
+        migrationsRun: true, // 애플리케이션 시작 시 마이그레이션 자동 실행
+        synchronize: false, // 엔티티와 데이터베이스 스키마 자동 동기화 비활성화
         extra: {
           ssl: process.env.NODE_ENV === 'production', // SSL 활성화 여부를 환경에 따라 결정
         },
---
+++ apps/backend/src/migrations/1713318480684-MyInitialMigration.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class MyInitialMigration1713318480684 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
