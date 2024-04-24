31f529cba45f3818e3dc20f99fd17df04018426e
HyeongSeoku <gudtjr3437@naver.com>
2024-04-24 16:18:29 +0900

    [BE] Test : 테스트 코드 의존성 주입 오류 수정

--- apps/backend/src/views/views.controller.spec.ts
+++ apps/backend/src/views/views.controller.spec.ts
@@ -1,12 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ViewsController } from './views.controller';
+import { ViewsService } from './views.service';
 
 describe('ViewsController', () => {
   let controller: ViewsController;
 
+  const mockViewsService = {
+    updatePostViewCount: jest.fn().mockResolvedValue(undefined),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ViewsController],
+      providers: [{ provide: ViewsService, useValue: mockViewsService }],
     }).compile();
 
     controller = module.get<ViewsController>(ViewsController);
