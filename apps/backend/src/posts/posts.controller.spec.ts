ac3d2cf1f3d006d713f467efefbecd08898e3560
HyeongSeoku <gudtjr3437@naver.com>
2024-04-24 17:19:36 +0900

    [BE] Test : Posts controller 테스트 코드 기본 작성

---
+++ apps/backend/src/posts/posts.controller.spec.ts
@@ -0,0 +1,39 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { PostsController } from './posts.controller';
+import { PostsService } from './posts.service';
+
+describe('PostsController', () => {
+  let controller: PostsController;
+  let postsService: PostsService;
+
+  const mockPostsService = {
+    findAll: jest.fn().mockReturnValue(undefined),
+    findOnePost: jest.fn().mockReturnValue(undefined),
+    createPost: jest.fn(),
+    updatePost: jest.fn(),
+    deletePost: jest.fn(),
+  };
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [PostsController],
+      providers: [{ provide: PostsService, useValue: mockPostsService }],
+    }).compile();
+
+    controller = module.get<PostsController>(PostsController);
+  });
+
+  afterAll(async () => {
+    jest.clearAllMocks();
+    jest.resetAllMocks();
+    jest.restoreAllMocks();
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
