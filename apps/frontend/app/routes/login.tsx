1f73ee3c1037fc2c23268ca57fbe20f1e9b0c706
HyeongSeok <gudtjr3437@naver.com>
2024-05-08 00:34:26 +0900

    [FE] Modify : github 로그인 로직 테스트 수정

--- apps/frontend/app/routes/login.tsx
+++ apps/frontend/app/routes/login.tsx
@@ -1,16 +1,27 @@
 import { MetaFunction } from "@remix-run/node";
-import { Link } from "@remix-run/react";
+import { Link, useNavigate } from "@remix-run/react";
 import { githubLogin } from "../../server/user";
+import { useEffect, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const meta: MetaFunction = () => {
   return [{ title: "login" }, { name: "login", content: "login" }];
 };
 
 export default function Index() {
-  const handleGithubLogin = async () => {
-    const response = await githubLogin();
+  const [state, setState] = useState("");
+  const navigate = useNavigate();
 
-    console.log("TEST", response);
+  useEffect(() => {
+    // state 값을 생성하여 상태에 저장
+    const newState = uuidv4();
+    setState(newState);
+    localStorage.setItem("oauth_state", newState);
+  }, []);
+
+  const handleGithubLogin = async () => {
+    // const response = await githubLogin(state);
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(`${import.meta.env.VITE_API_BASE_URL}/auth/github/callback`)}&scope=user:email&state=${state}`;
   };
 
   return (
--- apps/frontend/server/user.ts
+++ apps/frontend/server/user.ts
@@ -1,6 +1,10 @@
-import axiosClient from "./utils";
+import axios from "axios";
+// import axiosClient from "./utils";
 
-export const githubLogin = async () => {
+export const githubLogin = async (state: string) => {
   // FIXME: https://github.com/login/oauth/authorize?client_id=
-  return await axiosClient.get("/auth/github/login");
+  // return await axiosClient.get("/auth/github/login");
+  return await axios.get(
+    `https://github.com/login/oauth/authorize?client_id=${import.meta.env.VITE_GITHUB_CLIENT_ID}&redirect_uri=${encodeURIComponent(`${import.meta.env.VITE_API_BASE_URL}/auth/github/callback`)}&scope=user:email&state=${state}`
+  );
 };
