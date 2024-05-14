cb6aa3ea5c6f01e8f52b9525a97d4ceb9ba55c5d
HyeongSeoku <gudtjr3437@naver.com>
2024-05-14 15:15:57 +0900

    [BE] Modify : cookie 관련 util 메소드 추가

--- apps/backend/src/utils/cookie.ts
+++ apps/backend/src/utils/cookie.ts
@@ -1,11 +1,42 @@
 import { Response } from "express";
 import { isProdMode } from "./env";
 
+interface CookieOptions {
+  httpOnly?: boolean;
+  expires?: Date;
+  secure?: boolean;
+  sameSite?: "strict" | "lax" | "none";
+  path?: string;
+}
+
 export const clearCookie = (response: Response, cookieName: string) => {
   response.cookie(cookieName, "", {
     httpOnly: true,
     secure: isProdMode,
     sameSite: isProdMode ? "strict" : "lax",
     expires: new Date(0),
+  });
+};
+
+export const setCookie = (
+  res: Response,
+  key: string,
+  value: string,
+  options: CookieOptions = {},
+): void => {
+  const {
+    httpOnly = true,
+    expires,
+    secure = isProdMode,
+    sameSite = isProdMode ? "strict" : "lax",
+    path = "/",
+  } = options;
+
+  res.cookie(key, value, {
+    httpOnly,
+    expires,
+    secure,
+    sameSite,
+    path,
   });
 };
