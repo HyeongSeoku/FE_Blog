1ccb840cabd350b2e0a1c518dfdd0756047e80ea
HyeongSeoku <gudtjr3437@naver.com>
2024-05-27 20:59:16 +0900

    [BE] Modify : server-side 호출시 CORS 허용 대응

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -2,12 +2,20 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import * as cookieParser from "cookie-parser";
 import { AllExceptionsFilter } from "./exception";
+import { ConfigService } from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
+  const FE_BASE_URL = configService.get("FE_BASE_URL");
+
   app.use(cookieParser());
 
   app.useGlobalFilters(new AllExceptionsFilter());
+  app.enableCors({
+    origin: FE_BASE_URL,
+    credentials: true,
+  });
 
   await app.listen(3000);
 }
