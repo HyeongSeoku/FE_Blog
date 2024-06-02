2f2105da29e5e7fcb0cfde1a17ed7aa39c513d6b
HyeongSeok <gudtjr3437@naver.com>
2024-06-02 13:37:15 +0900

    [BE] Modify : github OAuth 의 정보에서 email이 없을 경우 github email api를 통해 email 가져오도록 수정

--- apps/backend/src/guards/strategy/github.strategy.ts
+++ apps/backend/src/guards/strategy/github.strategy.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
+import axios from "axios";
 import { Profile, Strategy } from "passport-github";
 import { GithubUserDto } from "src/users/dto/user.dto";
 import { UsersService } from "src/users/users.service";
@@ -27,8 +28,21 @@ export class GithubStrategy extends PassportStrategy(Strategy, "github") {
   ) {
     try {
       const { id, emails, photos, profileUrl, username } = profile;
+      let email = emails?.[0]?.value;
+      if (!email) {
+        // 이메일이 없는 경우, GitHub API를 통해 이메일 가져오기
+        const GITHUB_EMAIL_API_URL = "https://api.github.com/user/emails";
+        const emailResponse = await axios.get(GITHUB_EMAIL_API_URL, {
+          headers: {
+            Authorization: `token ${githubAccessToken}`,
+          },
+        });
+        const primaryEmail = emailResponse.data.find((email) => email.primary);
+        email = primaryEmail?.email ?? "";
+      }
+
       const githubDto: GithubUserDto = {
-        email: emails?.[0]?.value ?? "", // GitHub 프로필에서 이메일 주소 가져오기
+        email: email, // GitHub 프로필에서 이메일 주소 가져오기
         username,
         githubId: id,
         githubImgUrl: photos?.[0]?.value ?? "",
