f9d69e695dd03881130b4419f18dc94f74e192a5
HyeongSeok <gudtjr3437@naver.com>
2024-06-03 01:25:02 +0900

    [FE] Modify : 작성 페이지 수정

--- apps/frontend/app/routes/write._index.tsx
+++ apps/frontend/app/routes/write._index.tsx
@@ -1,5 +1,5 @@
-import { LoaderFunction, json, redirect } from "@remix-run/node";
-import { useState } from "react";
+import { LoaderFunction, json } from "@remix-run/node";
+import { useRef, useState } from "react";
 import ReactMarkdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import remarkGfm from "remark-gfm";
@@ -7,11 +7,7 @@ import useUserStore from "store/user";
 import { loaderCheckUser } from "utils/auth";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const { user, headers } = await loaderCheckUser(request);
-
-  if (!user?.isAdmin) {
-    redirect("/login");
-  }
+  const { user, headers } = await loaderCheckUser(request, true);
 
   return json(
     {
@@ -24,9 +20,11 @@ export const loader: LoaderFunction = async ({ request }) => {
 export default function Write() {
   const [markdown, setMarkdown] = useState("");
   const { userStore } = useUserStore();
+  const contentEditableRef = useRef<HTMLDivElement>(null);
 
-  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setMarkdown(e.target.value);
+  const handleChange = (e: React.SyntheticEvent) => {
+    const target = e.target as HTMLDivElement;
+    setMarkdown(target.innerText);
   };
 
   return (
@@ -35,22 +33,19 @@ export default function Write() {
       <div>{userStore.isAdmin}</div>
       <h1 className="text-3xl font-bold mb-4">글 작성</h1>
       <div className="flex flex-col md:flex-row">
-        <div className="w-full md:w-1/2 p-2">
-          <textarea
-            className="w-full h-96 p-2 border border-gray-300 rounded"
-            placeholder="Markdown으로 글을 작성하세요..."
-            value={markdown}
-            onChange={handleChange}
-          />
-        </div>
-        <div className="w-full md:w-1/2 p-2">
-          <div className="prose prose-lg p-4 border border-gray-300 rounded h-96 overflow-y-auto">
-            <ReactMarkdown
-              children={markdown}
-              remarkPlugins={[remarkGfm]}
-              rehypePlugins={[rehypeHighlight]}
-            />
-          </div>
+        <div
+          ref={contentEditableRef}
+          contentEditable
+          className="w-full h-96 p-2 border border-gray-300 rounded overflow-y-auto"
+          onInput={handleChange}
+        ></div>
+        <div className="w-full h-96 p-2 border border-gray-300 rounded overflow-y-auto">
+          <ReactMarkdown
+            remarkPlugins={[remarkGfm]}
+            rehypePlugins={[rehypeHighlight]}
+          >
+            {markdown}
+          </ReactMarkdown>
         </div>
       </div>
     </div>
