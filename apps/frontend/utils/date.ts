4bc0a7c53a86fcd36c28a3ede398fe5ddf7b76d2
HyeongSeoku <gudtjr3437@naver.com>
2024-07-03 13:38:36 +0900

    [FE] Modify : getDate 메소드

--- apps/frontend/utils/date.ts
+++ apps/frontend/utils/date.ts
@@ -1,11 +1,6 @@
 import dayjs from "dayjs";
 
-export interface GetDateParams {
-  date?: string;
-  format?: string;
-}
-
-export const getDate = ({ date, format = "YYYY/MM/DD" }: GetDateParams) => {
+export const getDate = (format: string = "YYYY/MM/DD", date?: string) => {
   const now = dayjs(date);
-  console.log(now);
+  return now.format(format);
 };
