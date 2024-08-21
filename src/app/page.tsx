026d78e59fc13ae266af1b26a050b5c7d89ed318
HyeongSeoku <gudtjr3437@naver.com>
2024-08-21 20:57:43 +0900

    Modify : 메인 레이아웃 작업중

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,16 @@ export default function Home() {
   return (
     <DefaultLayout>
       <div className="h-full">
-        <h1>Home page</h1>
+        <section className="flex flex-col items-center justify-center lg:text-3xl  xs:text-2xl pt-16 pb-20">
+          <h2>안녕하세요</h2>
+          <h2>을 추구하는</h2>
+          <h2>개발자 김형석입니다.</h2>
+        </section>
+
+        <section className="flex flex-col items-center justify-center lg:text-3xl  xs:text-2xl">
+          <h2>배우는 것에 즐거움을 느끼는</h2>
+          <h2>프론트엔드 개발자입니다.</h2>
+        </section>
       </div>
     </DefaultLayout>
   );
