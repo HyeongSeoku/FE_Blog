581e856c1e6a1c9dc81d66da6bd28fc72ccc1394
HyeongSeoku <gudtjr3437@naver.com>
2024-11-16 12:03:28 +0900

    Add : Mdx 링크 컴포넌트 추가

---
+++ src/components/MdxLink.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import { HTMLAttributeAnchorTarget, ReactNode } from "react";
+import ExternalIcon from "@/icon/external.svg";
+
+interface MdxLinkProps {
+  target?: HTMLAttributeAnchorTarget;
+  href?: string;
+  children: ReactNode;
+}
+
+const MdxLink = ({ href = "", target = "_self", children }: MdxLinkProps) => {
+  return (
+    <Link
+      href={href}
+      target={target}
+      rel="noopener noreferrer"
+      className="flex items-center"
+    >
+      {children}
+      <ExternalIcon width={16} height={16} />
+    </Link>
+  );
+};
+
+export default MdxLink;
