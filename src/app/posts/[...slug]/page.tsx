b94701eab81c511c1f5a5fe57e5a6f5e5ff9dc13
HyeongSeok <gudtjr3437@naver.com>
2024-11-22 01:21:31 +0900

    Modify : 게시물 상세 페이지 meta 데이터 세팅

--- src/app/posts/[...slug]/page.tsx
+++ src/app/posts/[...slug]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { redirect } from "next/navigation";
 import { getAllPosts, getPostsDetail } from "@/utils/mdxServer";
 import MdxDetailTemplate from "@/templates/MdxDetailTemplate/MdxDetailTemplate";
 
@@ -7,15 +7,57 @@ export async function generateStaticParams() {
   return posts.map((post) => ({ slug: post.slug.split("/") }));
 }
 
+async function getPostData(slug: string[]) {
+  const postData = await getPostsDetail(slug);
+
+  if (!postData) {
+    return null;
+  }
+
+  return postData;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string[] };
+}) {
+  const postData = await getPostData(params.slug);
+
+  if (!postData) {
+    return {
+      title: "게시글을 찾을 수 없습니다",
+      description: "요청하신 게시글이 존재하지 않습니다.",
+      other: { keyword: "" },
+      openGraph: {
+        publishedTime: "",
+      },
+    };
+  }
+
+  const { frontMatter } = postData;
+
+  return {
+    title: frontMatter.title || "SEOK 개발 블로그",
+    description:
+      frontMatter.description ||
+      "프론트엔드 개발자 김형석의 개발 블로그입니다.",
+    other: { keyword: frontMatter.tags?.join(",") || "" },
+    openGraph: {
+      publishedTime: frontMatter.createdAt || "",
+    },
+  };
+}
+
 export default async function PostPage({
   params,
 }: {
   params: { slug: string[] };
 }) {
-  const postData = await getPostsDetail(params.slug);
+  const postData = await getPostData(params.slug);
 
   if (!postData) {
-    notFound();
+    redirect("/not-found");
   }
 
   const { source, frontMatter, readingTime, heading } = postData;
