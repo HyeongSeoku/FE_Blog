6c9161b4fc8080ad880e7f5386cb2375780317e8
HyeongSeok <gudtjr3437@naver.com>
2024-11-25 02:52:21 +0900

    Add : AnimationContainer 컴포넌트 추가

---
+++ src/components/AnimationContainer.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { ElementType, ReactNode } from "react";
+import classNames from "classnames";
+import useAnimationVisibility from "@/hooks/useAnimationVisibility";
+import { ANIMAITE_FADE_IN_UP } from "@/constants/animation.constants";
+
+export type AnimationNameType =
+  | "animate-fadeInUp"
+  | "animate-fadeInDown"
+  | "animate-fadeInRight"
+  | "animate-fadeInLeft";
+
+interface AnimationContainerProps {
+  tag?: ElementType;
+  animationName?: AnimationNameType;
+  children: ReactNode;
+}
+
+const AnimationContainer = ({
+  tag: Tag = "div",
+  animationName = ANIMAITE_FADE_IN_UP,
+  children,
+  ...props
+}: AnimationContainerProps) => {
+  const [isVisible, ref] = useAnimationVisibility();
+
+  return (
+    <Tag
+      ref={ref}
+      className={classNames("opacity-0 transition duration-300", {
+        [animationName]: isVisible,
+      })}
+      {...props}
+    >
+      {children}
+    </Tag>
+  );
+};
+
+export default AnimationContainer;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,6 +51,22 @@ export default {
           "0%, 100%": { opacity: "1" },
           "50%": { opacity: "0" },
         },
+        fadeInUp: {
+          "0%": { opacity: "0", transform: "translateY(20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeInDown: {
+          "0%": { opacity: "0", transform: "translateY(-20px)" },
+          "100%": { opacity: "1", transform: "translateY(0)" },
+        },
+        fadeInLeft: {
+          "0%": { opacity: "0", transform: "translateX(-20px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
+        fadeInRight: {
+          "0%": { opacity: "0", transform: "translateX(20px)" },
+          "100%": { opacity: "1", transform: "translateX(0)" },
+        },
         bounceJelly: {
           "0%": {
             transform: "matrix(0, 0, 0, 0, 0, 0)",
@@ -164,6 +180,10 @@ export default {
         rotateFull: "rotateFull 3s linear infinite",
         rotateQuarter: "rotateQuarter 1s linear infinite",
         bounceJelly: "bounceJelly 1s linear both",
+        fadeInUp: "fadeInUp 0.3s ease-in-out forwards",
+        fadeInDown: "fadeInDown 0.3s ease-in-out forwards",
+        fadeInLeft: "fadeInLeft 0.3s ease-in-out forwards",
+        fadeInRight: "fadeInRight 0.3s ease-in-out forwards",
       },
     },
   },
