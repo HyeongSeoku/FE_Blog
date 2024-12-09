b0fc7bf3c1217972875fb2c103104bd0daf741fb
HyeongSeoku <gudtjr3437@naver.com>
2024-12-09 13:48:06 +0900

    Modify : PostCard 컴포넌트 반응형 적용

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -27,45 +27,46 @@ const PostCard = ({
   categoryType = "MAIN",
 }: PostCardProps) => {
   return (
-    <article className="w-64 h-80 box-border rounded-lg overflow-hidden shadow-md transform transition ease-in-out duration-300 hover:scale-102 md:w-full">
-      <Link href={link} className="block w-full h-full bg-white">
-        <div className="h-2/5 flex items-center justify-center p-3 relative overflow-hidden md:hidden">
-          <Image
-            src={imgSrc}
-            alt={imgAlt}
-            width={300}
-            height={300}
-            style={{ height: "100%", width: "auto", objectFit: "contain" }}
-          />
-        </div>
-        <div className="text-black px-5 py-4 h-3/5 flex flex-col justify-center">
-          <h3 className="text-xl font-semibold mt-1 ">{title}</h3>
-          <p className="text-sm mb-3 md:hidden">{description}</p>
-          {!!tags.length && (
-            <div className="md:hidden">
-              <div className="text-xs mb-1">태그</div>
-              <ul className="flex gap-1">
-                {tags.map((tagTitle, idx) => (
-                  <li
-                    key={`${idx}_${tagTitle}`}
-                    className="w-fit h-5 rounded-md border px-2 py-1 text-xs box-border flex items-center justify-center "
-                  >
-                    {tagTitle}
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
+    <Link
+      href={link}
+      className="flex flex-col w-64 h-80 box-border rounded-lg overflow-hidden shadow-md transform transition ease-in-out duration-300 hover:scale-102 bg-white md:w-full md:h-20 md:flex-row md:bg-transparent"
+    >
+      <div className="h-2/5 flex items-center justify-center p-3 relative overflow-hidden md:hidden">
+        <Image
+          src={imgSrc}
+          alt={imgAlt}
+          width={300}
+          height={300}
+          style={{ height: "100%", width: "auto", objectFit: "contain" }}
+        />
+      </div>
+      <div className="px-5 py-4 h-3/5 flex flex-col justify-center md:flex-row md:w-full md:h-full md:items-center md:justify-between min-md:text-black">
+        <h3 className="text-xl font-semibold mt-1 ">{title}</h3>
+        <p className="text-sm mb-3 md:hidden">{description}</p>
+        {!!tags.length && (
+          <div className="md:hidden">
+            <div className="text-xs mb-1">태그</div>
+            <ul className="flex gap-1">
+              {tags.map((tagTitle, idx) => (
+                <li
+                  key={`${idx}_${tagTitle}`}
+                  className="w-fit h-5 rounded-md border px-2 py-1 text-xs box-border flex items-center justify-center "
+                >
+                  {tagTitle}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
 
-          <time
-            dateTime={`${createdAt}`}
-            className="text-[var(--gray1-text-color)] text-xs"
-          >
-            {getDate("YYYY.MM.DD", `${createdAt}`)}
-          </time>
-        </div>
-      </Link>
-    </article>
+        <time
+          dateTime={`${createdAt}`}
+          className="text-[var(--gray1-text-color)] text-xs"
+        >
+          {getDate("YYYY.MM.DD", `${createdAt}`)}
+        </time>
+      </div>
+    </Link>
   );
 };
 
