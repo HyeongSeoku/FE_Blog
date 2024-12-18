60581573bcbbf6cb7b3f9262ed3351228be2a5f9
HyeongSeoku <gudtjr3437@naver.com>
2024-12-18 20:59:07 +0900

    Modify : useScrollProgress hook 마운트 시점에도 상태 업데이트하도록 수정

--- src/hooks/useScrollProgress.tsx
+++ src/hooks/useScrollProgress.tsx
@@ -11,7 +11,7 @@ const useScrollProgress = () => {
       const windowHeight =
         document.documentElement.scrollHeight - window.innerHeight;
       const progressValue = (scrollTop / windowHeight) * 100;
-      setProgress(progressValue);
+      setProgress(progressValue || 0);
     };
 
     const onScroll = () => {
@@ -23,6 +23,9 @@ const useScrollProgress = () => {
         });
       }
     };
+
+    // 초기 상태 업데이트
+    handleScroll();
 
     window.addEventListener("scroll", onScroll);
     return () => window.removeEventListener("scroll", onScroll);
