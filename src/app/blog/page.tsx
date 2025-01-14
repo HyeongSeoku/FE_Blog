a7428bd84d589279aa8e7792db9c594248cf4f27
HyeongSeoku <gudtjr3437@naver.com>
2025-01-14 13:40:18 +0900

    Modify : blog 페이지 SSG 적용 및 meta 데이터 추가

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,28 +1,39 @@
 import BlogPostCard from "@/components/BlogPostCard";
 import { getAllPosts } from "@/utils/post";
 
-const BlogPage = async () => {
-  const { postList } = await getAllPosts({
-    page: 1,
-    pageSize: 50,
-  });
+export const metadata = {
+  title: "블로그 페이지",
+  description: "최신 블로그 글 목록을 확인하세요.",
+};
 
-  return (
-    <div>
-      <ul>
-        {postList.map(({ title, createdAt, description, slug, tags }) => (
-          <BlogPostCard
-            key={slug}
-            title={title}
-            createdAt={createdAt}
-            description={description}
-            slug={slug}
-            tagList={tags}
-          />
-        ))}
-      </ul>
-    </div>
-  );
+export const generateStaticParams = async () => {
+  const { postList } = await getAllPosts({ page: 1, pageSize: 50 });
+  return postList.map((post) => ({ slug: post.slug }));
+};
+
+const BlogPage = async () => {
+  try {
+    const { postList } = await getAllPosts({ page: 1, pageSize: 50 });
+    return (
+      <div>
+        <ul>
+          {postList.map(({ title, createdAt, description, slug, tags }) => (
+            <BlogPostCard
+              key={slug}
+              title={title}
+              createdAt={createdAt}
+              description={description}
+              slug={slug}
+              tagList={tags}
+            />
+          ))}
+        </ul>
+      </div>
+    );
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+    return <div>포스트를 불러오는 중 오류가 발생했습니다.</div>;
+  }
 };
 
 export default BlogPage;
