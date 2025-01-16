377e923bbbb00094bef1bba0d7617e216d160e41
HyeongSeoku <gudtjr3437@naver.com>
2025-01-16 13:18:33 +0900

    Modify : BlogPostCard 컴포넌트 thumbnail 작업

--- src/components/BlogPostCard.tsx
+++ src/components/BlogPostCard.tsx
@@ -32,52 +32,63 @@ const BlogPostCard = ({
   const isReplace = replacePathList.some((path) => pathname.includes(path));
 
   return (
-    <li className="py-5 border-b last:border-b-0">
-      <Image width={100} height={100} src={thumbnail} alt="" />
-      <Link
-        replace={isReplace}
-        href={`/posts/${slug}`}
-        className="flex flex-col transition-colors duration-300 hover:text-gray-400"
-      >
-        <h3 className="text-3xl mb-1">{title}</h3>
+    <li className="py-5 border-b last:border-b-0 flex gap-4">
+      <Link href={`/posts/${slug}`} className="w-[150px] h-[150px] relative">
+        <Image
+          src={thumbnail}
+          alt="Post Thumbnail"
+          fill
+          className="bg-[var(--bg-gray-color)] rounded-md"
+          style={{ objectFit: "cover" }}
+          priority={true}
+        />
       </Link>
-      <div className="text-sm flex items-center gap-1">
+      <div className="flex-1">
         <Link
           replace={isReplace}
-          href={`/posts/month/${getDate("YYYY.MM", createdAt)}`}
-          className="flex flex-col transition-colors duration-300 hover:text-gray-400 mb-2"
+          href={`/posts/${slug}`}
+          className="flex flex-col transition-colors duration-300 hover:text-gray-400"
         >
-          {formattedMonth}
+          <h3 className="text-3xl mb-1">{title}</h3>
         </Link>
-        <Link
-          replace={isReplace}
-          href={`/posts/year/${getDate("YYYY", createdAt)}`}
-          className="flex flex-col transition-colors duration-300 hover:text-gray-400 mb-2"
-        >
-          {formattedYear}
-        </Link>
-      </div>
+        <div className="text-sm flex items-center gap-1">
+          <Link
+            replace={isReplace}
+            href={`/posts/month/${getDate("YYYY.MM", createdAt)}`}
+            className="flex flex-col transition-colors duration-300 hover:text-gray-400 mb-2"
+          >
+            {formattedMonth}
+          </Link>
+          <Link
+            replace={isReplace}
+            href={`/posts/year/${getDate("YYYY", createdAt)}`}
+            className="flex flex-col transition-colors duration-300 hover:text-gray-400 mb-2"
+          >
+            {formattedYear}
+          </Link>
+        </div>
 
-      <p className="text-md mb-2">{description}</p>
+        <p className="text-md mb-2">{description}</p>
 
-      {!!tagList?.length && (
-        <ul className="flex gap-2 text-sm ">
-          {tagList.map((item, idx) => (
-            <li
-              key={`${item}_${idx}`}
-              className="py-1 px-2 rounded-full transition-colors duration-300 bg-[var(--bg-gray-color)] hover:bg-[var(--bg-gray-hover-color)]"
-            >
-              <Link
-                replace={isReplace}
-                href={`/tags/${item.toLocaleLowerCase()}`}
-                className="w-full h-full block"
+        {!!tagList?.length && (
+          <ul className="flex gap-2 text-sm ">
+            {tagList.map((item, idx) => (
+              <li
+                key={`${item}_${idx}`}
+                className="py-1 px-2 rounded-full transition-colors duration-300 bg-[var(--bg-gray-color)] hover:bg-[var(--bg-gray-hover-color)]"
               >
-                {item}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      )}
+                <Link
+                  replace={isReplace}
+                  href={`/tags/${item.toLocaleLowerCase()}`}
+                  className="w-full h-full block"
+                >
+                  {item}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
     </li>
   );
 };
