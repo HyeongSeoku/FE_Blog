e2f5a94516f1b821ee923c3980d6b84011f2e502
HyeongSeoku <gudtjr3437@naver.com>
2025-03-04 13:23:45 +0900

    Modify : 메인페이지 모바일 모드 padding x 값 수정

--- src/app/pageClient.tsx
+++ src/app/pageClient.tsx
@@ -56,7 +56,7 @@ export const HomeClient = ({
   useDeviceType();
 
   return (
-    <div className="h-full flex flex-col gap-4 px-44 py-10 md-lg:px-32 md:px-12">
+    <div className="h-full flex flex-col gap-4 px-44 py-10 md-lg:px-32 md:px-5">
       <IntroSectionTemplate />
 
       <section className="flex flex-col gap-2">
