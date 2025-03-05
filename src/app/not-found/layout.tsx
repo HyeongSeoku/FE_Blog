11006b7d1648239826fa471ffeb2a1d69550fd3b
HyeongSeoku <gudtjr3437@naver.com>
2025-03-05 13:16:23 +0900

    Modify : 올바른 custom Notfound page 경로로 수정

--- src/app/not-found/layout.tsx
+++
@@ -1,9 +0,0 @@
-import NoneLayout from "@/layout/NoneLayout";
-
-export default function NotFoundLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <NoneLayout>{children}</NoneLayout>;
-}
