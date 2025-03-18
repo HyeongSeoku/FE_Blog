7d7b09dee8c6542d2325c4ed23d15a043a292acc
HyeongSeoku <gudtjr3437@naver.com>
2025-03-18 19:58:07 +0900

    Modify : not found 페이지 레이아웃에 footer 추가

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import NotFound from "@/components/NotFound";
 import NoneLayout from "@/layout/NoneLayout";
@@ -13,6 +14,7 @@ export default function Custom404() {
     <NoneLayout>
       <Header headerType="BACK" hideNavigation={true}></Header>
       <NotFound />
+      <Footer />
     </NoneLayout>
   );
 }
