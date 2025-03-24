04f46292ffca808046492f8cfa30e2b64550464b
HyeongSeoku <gudtjr3437@naver.com>
2025-03-24 13:39:21 +0900

    Modify : 카테고리별 색상값 순수 색상값만 반환되도록 레코드 수정

--- src/constants/style.constants.ts
+++ src/constants/style.constants.ts
@@ -1,15 +1,15 @@
 import { Category, SubCategory } from "@/types/posts";
 
 export const CATEGORY_COLORS: Record<Category | SubCategory, string> = {
-  DEV: "bg-[var(--category-dev-bg)]",
-  LIFE: "bg-[var(--category-life-bg)]",
-  ETC: "bg-[var(--category-etc-bg)]",
-  FE: "bg-[var(--category-fe-bg)]",
-  BE: "bg-[var(--category-be-bg)]",
-  DEV_OTHER: "bg-[var(--category-dev-other-bg)]",
-  WORK: "bg-[var(--category-work-bg)]",
-  HOBBY: "bg-[var(--category-hobby-bg)]",
-  BOOK: "bg-[var(--category-book-bg)]",
-  PHOTO: "bg-[var(--category-photo-bg)]",
-  MUSIC: "bg-[var(--category-music-bg)]",
+  DEV: "var(--category-dev-bg)",
+  LIFE: "var(--category-life-bg)",
+  ETC: "var(--category-etc-bg)",
+  FE: "var(--category-fe-bg)",
+  BE: "var(--category-be-bg)",
+  DEV_OTHER: "var(--category-dev-other-bg)",
+  WORK: "var(--category-work-bg)",
+  HOBBY: "var(--category-hobby-bg)",
+  BOOK: "var(--category-book-bg)",
+  PHOTO: "var(--category-photo-bg)",
+  MUSIC: "var(--category-music-bg)",
 };
