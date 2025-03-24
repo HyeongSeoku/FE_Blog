d61fd3d3eb718c6b8901b16b30313fd46f674215
HyeongSeoku <gudtjr3437@naver.com>
2025-03-24 13:40:12 +0900

    Modify : MainSection 컴포넌트 스타일 세부 조정

--- src/components/MainSection.tsx
+++ src/components/MainSection.tsx
@@ -12,7 +12,7 @@ const MainSection = ({ title, titleChildren, children }: MainSectionProps) => {
   return (
     <section className="my-10 flex flex-col justify-center">
       <div className="flex justify-between items-center">
-        <h3 className="text-lg font-bold">{title}</h3>
+        <h3 className="text-lg font-bold mb-2">{title}</h3>
         {titleChildren && titleChildren}
       </div>
       <div className="w-full h-fit">{children}</div>
