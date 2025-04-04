77973873172082ca20eb9f1632c59f44ae45aceb
HyeongSeoku <gudtjr3437@naver.com>
2025-04-04 13:05:49 +0900

    Modify : Prettier devDependency 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,42 +5,41 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: './tsconfig.json',
-    sourceType: 'module',
+    project: "./tsconfig.json",
+    sourceType: "module",
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ["@typescript-eslint", "prettier"],
   extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking', 
-    'plugin:prettier/recommended',
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "plugin:prettier/recommended",
   ],
   rules: {
-    'prettier/prettier': 'error',
+    "prettier/prettier": "error",
 
     // 🔒 타입 안전성
-    '@typescript-eslint/no-explicit-any': 'warn',
-    '@typescript-eslint/no-non-null-assertion': 'error',
-    '@typescript-eslint/no-floating-promises': 'error',
-    '@typescript-eslint/no-misused-promises': 'error',
-    '@typescript-eslint/no-unsafe-return': 'warn',
-    
-    '@typescript-eslint/no-unsafe-assignment': 'warn',
-    '@typescript-eslint/no-unsafe-member-access': 'warn',
-    '@typescript-eslint/no-unsafe-call': 'warn',
-    '@typescript-eslint/no-unsafe-argument': 'warn',
-  
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-non-null-assertion": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-misused-promises": "error",
+    "@typescript-eslint/no-unsafe-return": "warn",
+
+    "@typescript-eslint/no-unsafe-assignment": "warn",
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-unsafe-call": "warn",
+    "@typescript-eslint/no-unsafe-argument": "warn",
 
     // 🧹 불필요한 변수 제거
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
     ],
 
     // ✅ 기타 스타일 관련
-    'no-console': ['error', { allow: ['warn', 'error'] }],
-    'no-debugger': 'error',
+    "no-console": ["error", { allow: ["warn", "error"] }],
+    "no-debugger": "error",
   },
-  ignorePatterns: ['.eslintrc.js', 'next.config.js', 'dist/', '.next/'],
+  ignorePatterns: [".eslintrc.js", "next.config.js", "dist/", ".next/"],
 };
