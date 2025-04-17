6a423b9460b9d22d5a1038d6f35821f41b0a899a
HyeongSeoku <gudtjr3437@naver.com>
2025-04-17 12:57:10 +0900

    Modify : lighthouse upload 방식 재수정 및 .env 키 추가

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -5,11 +5,6 @@ module.exports = {
       url: ["http://localhost:3000"],
       numberOfRuns: 1,
     },
-    // upload: {
-    //   target: "filesystem",
-    //   outputDir: "./.lighthouseci",
-    //   reportFilenamePattern: "report.html",
-    // },
     upload: {
       target: "temporary-public-storage",
     },
