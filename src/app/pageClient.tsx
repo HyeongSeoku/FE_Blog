cb5a23bff25bf15ff92ef056e1fe9b6ca7134c7e
HyeongSeoku <gudtjr3437@naver.com>
2025-05-20 12:42:08 +0900

    Modify : 스킬칩 클릭시 임시로 넣어둔 모달 노출 제거

--- src/app/pageClient.tsx
+++ src/app/pageClient.tsx
@@ -3,16 +3,11 @@
 import useDeviceType from "@/hooks/useDeviceType";
 import { PostDataProps } from "@/types/posts";
 
-import { useEffect, useMemo, useState } from "react";
-import dynamic from "next/dynamic";
+import { useEffect } from "react";
 import { GithubUserInfo } from "@/api/github";
 import useGithubInfoStore from "@/store/githubInfo";
 import SkillChip from "@/components/SkillChip";
 import { SKILL_LIST } from "@/constants/post.constants";
-
-const Modal = dynamic(() => import("@/components/Modal/Modal"), {
-  ssr: false,
-});
 
 interface HomeClientProps {
   postList: PostDataProps[];
@@ -21,20 +16,7 @@ interface HomeClientProps {
 }
 
 export const HomeClient = ({ githubData }: HomeClientProps) => {
-  const [targetSkill, setTargetSkill] = useState("");
   const { setGithubUser } = useGithubInfoStore();
-
-  const isOpen = useMemo(() => {
-    return !!targetSkill;
-  }, [targetSkill]);
-
-  const onClickSkillChip = (skillName: string) => {
-    setTargetSkill(skillName);
-  };
-
-  const onCloseSkillModal = () => {
-    setTargetSkill("");
-  };
 
   useEffect(() => {
     setGithubUser({
@@ -58,21 +40,10 @@ export const HomeClient = ({ githubData }: HomeClientProps) => {
               backGroundColor={bgColor}
               imgSrc={imgSrc}
               index={idx + 1}
-              onClick={() => onClickSkillChip(skillName)}
             />
           ))}
         </ul>
       </section>
-
-      <Modal
-        title={targetSkill}
-        isOpen={isOpen}
-        hasCloseBtn={true}
-        onClose={onCloseSkillModal}
-        className="w-1/2"
-      >
-        test
-      </Modal>
     </div>
   );
 };
