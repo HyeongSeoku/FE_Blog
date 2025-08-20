eac2b34150f4ac9b69e4843ab2dbf92a30c2c5f4
HyeongSeoku <gudtjr3437@naver.com>
2025-08-20 13:03:53 +0900

    Modify : 배포시 sitemap 반영되지 않는 cwd 경로 이슈 수정

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "seoku-blog",
       script: "pnpm",
       args: "start",
-      cwd: "/my-app/seoku-blog",
+      cwd: "/home/ubuntu/actions-runner/_work/FE_Blog/FE_Blog/release",
       env: {
         PORT: 3000,
       },
