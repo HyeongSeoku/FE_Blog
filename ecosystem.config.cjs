d9ed792f1d4981a27753b46ad0df6c753f972d71
HyeongSeoku <gudtjr3437@naver.com>
2025-08-20 13:20:31 +0900

    Modify : pnpm을 경유하지 않고 직접 바이너리 실행하도록 수정

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: "seoku-blog",
-      script: "pnpm",
-      args: "start",
+      script: "./node_modules/next/dist/bin/next",
+      args: "start -p 3000",
       cwd: "/home/ubuntu/actions-runner/_work/FE_Blog/FE_Blog/release",
       env: {
         PORT: 3000,
