9c1332b6a04d0b63674d8e97053fdae47657bb60
HyeongSeoku <gudtjr3437@naver.com>
2026-04-08 11:51:25 +0900

    fix : build 에러 수정

--- src/constants/basic.constants.ts
+++ src/constants/basic.constants.ts
@@ -1,27 +1,10 @@
-import path from "node:path";
-
 export const PUBLIC_IMG_PATH = "/image";
 export const PUBLIC_CONTENT_IMG_PATH = "/content-image";
 
 export const MOBILE_WIDTH = 768;
 export const GISCUS_SECTION = "giscusSection";
 
-// 이미지 경로와 설정
-export const CONTENT_IMAGE_DIR = path.join(
-  process.cwd(),
-  "public/content-image",
-);
 export const DEFAULT_MDX_PATH = "src/mdx";
-export const PROJECT_PATH = path.join(
-  process.cwd(),
-  `${DEFAULT_MDX_PATH}/project`,
-);
-export const POST_PATH = path.join(
-  process.cwd(),
-  `${DEFAULT_MDX_PATH}/content`,
-);
-
-export const SKILL_PATH = path.join(process.cwd(), `${DEFAULT_MDX_PATH}/skill`);
 export const DEFAULT_POST_THUMBNAIL = `${PUBLIC_IMG_PATH}/default_img.webp`;
 
 export const BASE_URL =
--- src/constants/post.constants.ts
+++ src/constants/post.constants.ts
@@ -1,46 +1,5 @@
-import type { SkillName } from "@/components/SkillChip";
 import type { Category, SubCategory } from "@/types/posts";
-import { PUBLIC_IMG_PATH, SKILL_PATH } from "./basic.constants";
 import { CATEGORY_COLORS } from "./style.constants";
-
-export const SKILL_LIST: {
-  skillName: SkillName;
-  bgColor?: `#${string}`;
-  imgSrc: string;
-  contentPath: string;
-}[] = [
-  {
-    skillName: "React",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/react.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-  {
-    skillName: "TypeScript",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/typescript.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-  {
-    skillName: "Next.js",
-    bgColor: "#363636",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/nextjs.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-  {
-    skillName: "JavaScript",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/javascript.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-  {
-    skillName: "HTML",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/html5.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-  {
-    skillName: "CSS",
-    imgSrc: `${PUBLIC_IMG_PATH}/skill/css3.svg`,
-    contentPath: `${SKILL_PATH}`,
-  },
-];
 
 export const CATEGORY_MAP: Record<
   Category,
