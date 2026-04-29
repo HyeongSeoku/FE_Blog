baecb3da4f606fa46c8c0395a0819d7aa6674519
HyeongSeoku <gudtjr3437@naver.com>
2026-04-29 17:41:14 +0900

    fix: theme 관련 Hydration 이슈 수정

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,6 +1,6 @@
 import { create } from "zustand";
 import { LIGHT_DARK_THEME } from "@/constants/cookie.constants";
-import { setCookie } from "@/utils/cookies";
+import { getCookie, setCookie } from "@/utils/cookies";
 
 interface ThemeState {
   isDarkMode: boolean;
@@ -9,36 +9,30 @@ interface ThemeState {
   setLightMode: () => void;
 }
 
-const getInitialTheme = (): boolean => {
-  if (typeof document !== "undefined") {
-    const htmlTheme = document.documentElement.getAttribute("data-theme");
-    if (htmlTheme === "dark") return true;
-    if (htmlTheme === "light") return false;
-
-    return window.matchMedia("(prefers-color-scheme: dark)").matches;
-  }
-
-  return false;
+const getInitialIsDarkMode = (): boolean => {
+  if (typeof window === "undefined") return false;
+  const cookieTheme = getCookie(LIGHT_DARK_THEME);
+  if (cookieTheme === "dark") return true;
+  if (cookieTheme === "light") return false;
+  return window.matchMedia("(prefers-color-scheme: dark)").matches;
 };
 
-const useThemeStore = create<ThemeState>((set) => {
-  return {
-    isDarkMode: getInitialTheme(),
-    toggleTheme: () =>
-      set((state) => {
-        const newTheme = !state.isDarkMode;
-        setCookie(LIGHT_DARK_THEME, newTheme ? "dark" : "light", 365);
-        return { isDarkMode: newTheme };
-      }),
-    setDarkMode: () => {
-      setCookie(LIGHT_DARK_THEME, "dark", 365);
-      set({ isDarkMode: true });
-    },
-    setLightMode: () => {
-      setCookie(LIGHT_DARK_THEME, "light", 365);
-      set({ isDarkMode: false });
-    },
-  };
-});
+const useThemeStore = create<ThemeState>((set) => ({
+  isDarkMode: getInitialIsDarkMode(),
+  toggleTheme: () =>
+    set((state) => {
+      const newTheme = !state.isDarkMode;
+      setCookie(LIGHT_DARK_THEME, newTheme ? "dark" : "light", 365);
+      return { isDarkMode: newTheme };
+    }),
+  setDarkMode: () => {
+    setCookie(LIGHT_DARK_THEME, "dark", 365);
+    set({ isDarkMode: true });
+  },
+  setLightMode: () => {
+    setCookie(LIGHT_DARK_THEME, "light", 365);
+    set({ isDarkMode: false });
+  },
+}));
 
 export default useThemeStore;
